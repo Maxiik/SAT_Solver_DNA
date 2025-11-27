f3d7f84d19a6357e99871fa851b2370dd0d282b4
Maximilian Jan Furman <max.furman@live.com>
2025-11-27 05:59:33 +0100

    Add: Skeleton of the main program

---
+++ word_design_dna.py
@@ -0,0 +1,76 @@
+import subprocess
+from argparse import ArgumentParser
+
+def load_instance():
+    return
+
+def encode():
+    return
+
+def print_result():
+    return
+
+def call_solver(cnf, nr_vars, output_name, solver_name, verbosity):
+    # print CNF into formula.cnf in DIMACS format
+    with open(output_name, "w") as file:
+        file.write("p cnf " + str(nr_vars) + " " + str(len(cnf)) + '\n')
+        for clause in cnf:
+            file.write(' '.join(str(lit) for lit in clause) + '\n')
+
+    # call the solver and return the output
+    return subprocess.run(['./' + solver_name, '-model', '-verb=' + str(verbosity) , output_name], stdout=subprocess.PIPE)
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "-i",
+        "--input",
+        default="input.in",
+        type=str,
+        help=(
+            "The instance file."
+        ),
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        default="formula.cnf",
+        type=str,
+        help=(
+            "Output file for the DIMACS format (i.e. the CNF formula)."
+        ),
+    )
+    parser.add_argument(
+        "-s",
+        "--solver",
+        default="glucose-syrup",
+        type=str,
+        help=(
+            "The SAT solver to be used."
+        ),
+    )
+    parser.add_argument(
+        "-v",
+        "--verb",
+        default=1,
+        type=int,
+        choices=range(0,2),
+        help=(
+            "Verbosity of the SAT solver used."
+        ),
+    )
+    args = parser.parse_args()
+
+    # get the input instance
+    instance = load_instance(args.input)
+
+    # encode the problem to create CNF formula
+    cnf, nr_vars = encode(instance)
+
+    # call the SAT solver and get the result
+    result = call_solver(cnf, nr_vars, args.output, args.solver, args.verb)
+
+    # interpret the result and print it in a human-readable format
+    print_result(result)
